526b0da76c6d6d7adbc311b697b2bc63e9d5d915
Fagner Pimentel <fagnerpimentel@gmail.com>
2026-06-19 23:19:12 -0300

    Update event details for RoboCup@Home Brazil Open 2026

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Competição Brasileira de Robótica (CBR) / RoboCup@home Brazil Open  
 
-[https://robocupathomelatinamerica.github.io/YEAR_LOCAL/](https://robocupathomelatinamerica.github.io/YEAR_LOCAL/)
+[https://robocupathomelatinamerica.github.io/2026_JoaoPessoa/](https://robocupathomelatinamerica.github.io/2026_JoaoPessoa/)
--- scripts/data.js
+++ scripts/data.js
@@ -1,11 +1,10 @@
 const info = {
-  year: "2025",
-  date: "October 13 to 19",
-  city: "Vitória - Espírito Santo",
+  year: "2026",
+  date: "November 23 to 29",
+  city: "João Pessoa - Paraíba",
   chair: "Fagner de Assis Moura Pimentel <fagnerpimentel@gmail.com>",
   oc: [
     "Ana Patricia Magalhães (UNEB/UNIFACS)",
-    "Pedro Edom Nunes (FURG)",
     "Gabriela Bassegio (FEI)",
     "Gustavo Fardo Armênio (UTFPR)",
     "Alexandre Costa (UFG)",
