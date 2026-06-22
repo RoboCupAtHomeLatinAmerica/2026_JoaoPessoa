b746cb1d7808da78d756c1b92fce932e9cc4f2ad
Fagner Pimentel <fagnerpimentel@gmail.com>
2026-06-22 15:24:10 -0300

    Refactor team chair information and update event dates in HTML and JavaScript files

--- scripts/data.js
+++ scripts/data.js
@@ -2,9 +2,11 @@ const info = {
   year: "2026",
   date: "November 23 to 29",
   city: "João Pessoa - Paraíba",
-  chair: "Fagner de Assis Moura Pimentel <fagnerpimentel@gmail.com>",
+  chair: [
+    "Fagner de Assis Moura Pimentel <fagnerpimentel@gmail.com>",
+    "Ana Patricia Magalhães (UNEB)",
+  ],
   oc: [
-    "Ana Patricia Magalhães (UNEB/UNIFACS)",
     "Gabriela Bassegio (FEI)",
     "Gustavo Fardo Armênio (UTFPR)",
     "Alexandre Costa (UFG)",
